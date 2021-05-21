d9e7b263795343ae26ca8ae4b73cb3447a572acc
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-21 11:43:39 +0800

    add blog details style and render rich text content

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from 'contentful';
+import Image from 'next/image';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 const client = createClient({
 	space: process.env.CONTENTFUL_SPACE_ID,
@@ -34,7 +36,53 @@ export async function getStaticProps({ params }) {
 }
 
 export default function BlogDetails({ blog }) {
-	console.log(blog);
-
-	return <div>Blog Details</div>;
+	const { featuredImage, title, tags, content } = blog.fields;
+	console.log(tags);
+	return (
+		<div>
+			<div className='banner'>
+				<Image
+					src={'https:' + featuredImage.fields.file.url}
+					width={featuredImage.fields.file.details.image.width}
+					height={featuredImage.fields.file.details.image.height}
+				/>
+				<h2>{title}</h2>
+			</div>
+			<div className='info'>
+				<p>Tags: </p>
+				{tags.map((tag) => (
+					<span key={tag}>{tag}</span>
+				))}
+			</div>
+			<div className='content'>
+				<div>{documentToReactComponents(content)}</div>
+			</div>
+			<style jsx>{`
+				h2,
+				h3 {
+					text-transform: uppercase;
+				}
+				.banner h2 {
+					margin: 0;
+					background: #fff;
+					display: inline-block;
+					padding: 20px;
+					position: relative;
+					top: -60px;
+					left: -10px;
+					transform: rotateZ(-1deg);
+					box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+				}
+				.info p {
+					margin: 0;
+				}
+				.info span::after {
+					content: ', ';
+				}
+				.info span:last-child::after {
+					content: ' ';
+				}
+			`}</style>
+		</div>
+	);
 }
