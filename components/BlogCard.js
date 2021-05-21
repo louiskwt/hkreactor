185af669db144a9e8239c236094977215f3a03e4
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-21 10:50:53 +0800

    using images from contentful

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,11 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const BlogCard = ({ blog }) => {
 	const { title, slug, thumbnail } = blog.fields;
 
 	return (
 		<div className='card'>
-			<div className='featured'>{/* Image */}</div>
+			<div className='featured'>
+				<Image
+					src={'https:' + thumbnail.fields.file.url}
+					width={thumbnail.fields.file.details.image.width}
+					height={thumbnail.fields.file.details.image.height}
+				/>
+			</div>
 			<div className='content'>
 				<div className='info'>
 					<h4>{title}</h4>
