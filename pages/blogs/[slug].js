d72ca62222478871869386be743ac388bc9bb019
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-21 11:19:21 +0800

    generating path for each blog

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,3 +1,40 @@
-export default function RecipeDetails() {
+import { createClient } from 'contentful';
+
+const client = createClient({
+	space: process.env.CONTENTFUL_SPACE_ID,
+	accessToken: process.env.CONTENTFUL_ACCESS_KEY
+});
+
+export const getStaticPaths = async () => {
+	const res = await client.getEntries({
+		content_type: 'blog'
+	});
+
+	const paths = res.items.map((item) => {
+		return {
+			params: { slug: item.fields.slug }
+		};
+	});
+
+	return {
+		paths,
+		fallback: false
+	};
+};
+
+export async function getStaticProps({ params }) {
+	const { items } = await client.getEntries({
+		content_type: 'blog',
+		'fields.slug': params.slug
+	});
+
+	return {
+		props: { blog: items[0] }
+	};
+}
+
+export default function BlogDetails({ blog }) {
+	console.log(blog);
+
 	return <div>Blog Details</div>;
 }
